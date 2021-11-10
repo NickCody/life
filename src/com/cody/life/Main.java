c7b1efd1c2e3be5405dce22c4bd9be42c98b88c2
codya <codya100@gmail.com>
2021-11-10 17:52:29 -0500

    update

--- src/com/cody/life/Main.java
+++ src/com/cody/life/Main.java
@@ -3,7 +3,15 @@ package com.cody.life;
 public class Main {
 
     public static void main(String[] args) {
-        Board board = new Board(10,10);
-        board.print();
+        Board board = new Board(20,20);
+        board.random();
+        System.out.println("This is the random board");
+        board.debugPrint();
+
+        board.newGeneration();
+        System.out.println("This is the random board with game of life applied");
+        board.debugPrint();
+
+
     }
 }
