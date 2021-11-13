fcdda2589f3568963c450b3f7beaf735d55aa377
nic <nick.codignotto@gmail.com>
2021-11-12 21:59:36 -0500

    With primitive screen clearing

--- src/main/java/com/cody/life/immutable/Run.java
+++ src/main/java/com/cody/life/immutable/Run.java
@@ -4,10 +4,10 @@ import com.cody.life.immutable.board.LifeBoard;
 
 public class Run {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
-        int rows = 50;
-        int cols = 50;
+        int rows = 20;
+        int cols = 20;
 
         if (args.length == 1) {
             rows = cols = Integer.parseInt(args[0]);
@@ -25,19 +25,23 @@ public class Run {
         boolean same = false;
 
         while (numAlive > 0 && !same) {
+            System.out.print("\033[H\033[2J");
+            System.out.flush();
 
             // Generate new generation and update stats
             //
-            LifeBoard newGeneration = board.debugPrint().generate().printTiming("Time for generation " + generation++);
+            LifeBoard newGeneration = board.print().generate().printTiming("Time for generation " + generation++);
 
             same = board.isSameBoard(newGeneration);
             board = newGeneration;
             numAlive = board.numAlive();
+
+            Thread.sleep(100);
         }
 
         if (same) {
             System.out.println("Detected identical generation at generation " + generation + "!");
-            board.debugPrint();
+            board.print();
         } else if (numAlive == 0) {
             System.out.println("Detected extinction at generation " + generation + "!");
             board.debugPrint();
